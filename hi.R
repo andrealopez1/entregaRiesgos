43bb85d1f96ad694468f03fe6ca6c76b381fa302
turme <43544987+turme@users.noreply.github.com>
2018-09-25 11:01:00 -0500

    Código funionando

--- hi.R
+++ hi.R
@@ -4,15 +4,17 @@ author: "Riesgos"
 date: "24 de sep de 2018"
 output: html_document
 ---
-  Estabilidad <- t(matrix(c(1,7,7,1,1/7,1,1,1/7,1/7,1,1,1/7,1,7,7,1),nrow=4,ncol=4))
-colnames(Estabilidad) = c("c1","c2","c3","c4")
-rownames(Estabilidad) = c("c1","c2","c3","c4")
+ 
 
 Edad<- t(matrix(c(1,2,1/3,7,1/2,1,1/5,5,3,5,1,9,1,1/5,1/9,1),nrow=4,ncol=4))
 colnames(Edad) = c("c1","c2","c3","c4")
 rownames(Edad) = c("c1","c2","c3","c4")
 
-Codeudor <- t(matrix(c(1,1,8,8,1,1,8,8,1/8,1/8,1,9,1/8,1/8,1,1),nrow=4,ncol=4))
+Edad<- t(matrix(c(1,2,1/3,7,1/2,1,1/5,5,3,5,1,9,1/7,1/5,1/9,1),nrow=4,ncol=4))
+colnames(Edad) = c("c1","c2","c3","c4")
+rownames(Edad) = c("c1","c2","c3","c4")
+
+Codeudor <- t(matrix(c(1,1,8,8,1,1,8,8,1/8,1/8,1,9,1/8,1/8,1/9,1),nrow=4,ncol=4))
 colnames(Codeudor) = c("c1","c2","c3","c4")
 rownames(Codeudor) = c("c1","c2","c3","c4")
 
@@ -28,12 +30,15 @@ Salario <- t(matrix(c(1,3,3,2,1/3,1,1,1/2,1/3,1,1,1/2,1/2,2,2,1),nrow=4,ncol=4))
 colnames(Salario) = c("c1","c2","c3","c4")
 rownames(Salario) = c("c1","c2","c3","c4")
 
-Patrimonio <- t(matrix(c(1,8,6,1,1/8,1,1/2,1,1/6,2,1,1/6,1,8,6,1),nrow=4,ncol=4))
+Patrimonio <- t(matrix(c(1,8,6,1,1/8,1,1/2,1/8,1/6,2,1,1/6,1,8,6,1),nrow=4,ncol=4))
 colnames(Patrimonio) = c("c1","c2","c3","c4")
 rownames(Patrimonio) = c("c1","c2","c3","c4")
 
+Est <- t(matrix(c(1,7,7,1,1/7,1,1,1/7,1/7,1,1,1/7,1,7,7,1),nrow=4,ncol=4))
+colnames(Patrimonio) = c("c1","c2","c3","c4")
+rownames(Patrimonio) = c("c1","c2","c3","c4")
 
-alternativesPairwiseComparisonsList <- list(Estabilidad=Estabilidad,
+alternativesPairwiseComparisonsList <- list(Est=Est,
                                             Edad=Edad,
                                             Codeudor=Codeudor,
                                             Obligaciones=Obligaciones,
@@ -41,11 +46,12 @@ alternativesPairwiseComparisonsList <- list(Estabilidad=Estabilidad,
                                             Salario=Salario,
                                             Patrimonio=Patrimonio)
 
-criteriaWeightsPairwiseComparisons <- t(matrix(c(1,1/3,1/5,4,1/2,2,1/6,3,1,1/6,2,1/4,3,8,5,6,1,7,2,5,1/3,1,1/2,1/7,1,1,1/2,1/6,2,4,1/2,1,1,1,1/2,1/2,1/3,1/5,2,1,1,1/5,6,1/8,3,6,2,5,1),
+criteriaWeightsPairwiseComparisons <- t(matrix(c(1,1/3,1/5,4,1/2,2,1/6,3,1,1/6,2,1/4,3,8,5,6,1,7,2,5,1/3,1/4,1/2,1/7,1,1,1/2,1/6,2,4,1/2,1,1,1,1/2,1/2,1/3,1/5,2,1,1,1/5,6,1/8,3,6,2,5,1),
                                                nrow=7,ncol=7))
-colnames(criteriaWeightsPairwiseComparisons) = c("Estabilidad","Edad","Codeudor","Obligaciones","Capacidad_cred","Salario","Patrimonio")
-rownames(criteriaWeightsPairwiseComparisons) = c("Estabilidad","Edad","Codeudor","Obligaciones","Capacidad_cred","Salario","Patrimonio")
+colnames(criteriaWeightsPairwiseComparisons) = c("Est","Edad","Codeudor","Obligaciones","Capacidad_cred","Salario","Patrimonio")
+rownames(criteriaWeightsPairwiseComparisons) = c("Est","Edad","Codeudor","Obligaciones","Capacidad_cred","Salario","Patrimonio")
 
 overall1 <- AHP(criteriaWeightsPairwiseComparisons,alternativesPairwiseComparisonsList)
 
 overall1
+
