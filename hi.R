07594663461ac2ca968c10b3bb2c6b3a503a745b
turme <43544987+turme@users.noreply.github.com>
2018-09-24 20:58:31 -0500

    c4

--- hi.R
+++ hi.R
@@ -4,34 +4,33 @@ author: "Riesgos"
 date: "24 de sep de 2018"
 output: html_document
 ---
-  
-Estabilidad <- t(matrix(c(1,7,7,1,1/7,1,1,1/7,1/7,1,1,1/7,1,7,7,1),nrow=4,ncol=4))
-colnames(Estabilidad) = c("c1","c2","c3")
-rownames(Estabilidad) = c("c1","c2","c3")
+  Estabilidad <- t(matrix(c(1,7,7,1,1/7,1,1,1/7,1/7,1,1,1/7,1,7,7,1),nrow=4,ncol=4))
+colnames(Estabilidad) = c("c1","c2","c3","c4")
+rownames(Estabilidad) = c("c1","c2","c3","c4")
 
 Edad<- t(matrix(c(1,2,1/3,7,1/2,1,1/5,5,3,5,1,9,1,1/5,1/9,1),nrow=4,ncol=4))
-colnames(Edad) = c("c1","c2","c3")
-rownames(Edad) = c("c1","c2","c3")
+colnames(Edad) = c("c1","c2","c3","c4")
+rownames(Edad) = c("c1","c2","c3","c4")
 
 Codeudor <- t(matrix(c(1,1,8,8,1,1,8,8,1/8,1/8,1,9,1/8,1/8,1,1),nrow=4,ncol=4))
-colnames(Codeudor) = c("c1","c","c")
-rownames(Codeudor) = c("c1","c2","c3")
+colnames(Codeudor) = c("c1","c2","c3","c4")
+rownames(Codeudor) = c("c1","c2","c3","c4")
 
 Obligaciones <- t(matrix(c(1,1,3,5,1,1,2,3,1/3,1/2,1,2,1/5,1/3,1/2,1),nrow=4,ncol=4))
-colnames(Obligaciones) = c("c1","c2","c3")
-rownames(Obligaciones) = c("c1","c2","c3")
+colnames(Obligaciones) = c("c1","c2","c3","c4")
+rownames(Obligaciones) = c("c1","c2","c3","c4")
 
 Capacidad_cred <- t(matrix(c(1,6,1,4,1/6,1,1/6,1/2,1,6,1,4,1/4,2,1/4,1),nrow=4,ncol=4))
-colnames(Capacidad_cred) = c("c1","c2","c3")
-rownames(Capacidad_cred) = c("c1","c2","c3")
+colnames(Capacidad_cred) = c("c1","c2","c3","c4")
+rownames(Capacidad_cred) = c("c1","c2","c3","c4")
 
 Salario <- t(matrix(c(1,3,3,2,1/3,1,1,1/2,1/3,1,1,1/2,1/2,2,2,1),nrow=4,ncol=4))
-colnames(Salario) = c("c1","c2","c3")
-rownames(Salario) = c("c1","c2","c3")
+colnames(Salario) = c("c1","c2","c3","c4")
+rownames(Salario) = c("c1","c2","c3","c4")
 
 Patrimonio <- t(matrix(c(1,8,6,1,1/8,1,1/2,1,1/6,2,1,1/6,1,8,6,1),nrow=4,ncol=4))
-colnames(Patrimonio) = c("c1","c2","c3")
-rownames(Patrimonio) = c("c1","c2","c3")
+colnames(Patrimonio) = c("c1","c2","c3","c4")
+rownames(Patrimonio) = c("c1","c2","c3","c4")
 
 
 alternativesPairwiseComparisonsList <- list(Estabilidad=Estabilidad,
