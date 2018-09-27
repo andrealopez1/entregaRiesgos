3843213887ec03eb5e2d3b6991f790c9d18f78a2
Andrea <andre_lopez01@yahoo.com>
2018-09-27 17:38:19 -0500

    Parte de las gráficas

--- hi.R
+++ hi.R
@@ -46,3 +46,117 @@ overall1 <- AHP(criteriaWeightsPairwiseComparisons,alternativesPairwiseCompariso
 
 overall1
 
+
+
+
+
+
+
+
+
+#Formar tabla de respuestas 
+respuestas<-c(1,2,2,1,2,3,1,5,1,1,2,2,3,3,4,5,4,2,4,3,3,1,1,2,4,1,2,4)
+datos<-matrix(data=respuestas, ncol=4,nrow=7,byrow=TRUE)
+colnames(datos) = c("c1","c2","c3","c4")
+rownames(datos) = c("Estabilidad laboral","Edad","Codeudor","Obligaciones","Capacidad crediticia","Salario","Patrimonio")
+View(datos)
+
+#Separar las filas de la tabla en vectores
+estabilidadLaboral<- as.numeric(datos[1, ])
+edad              <- as.numeric(datos[2, ])
+codeudor          <- as.numeric(datos[3, ])
+obligaciones      <- as.numeric(datos[4, ])
+capacidadCreditica<- as.numeric(datos[5, ])
+salario           <- as.numeric(datos[6, ])
+patrimonio        <- as.numeric(datos[7, ])
+#Añadir nombre a los elementos del vector
+n <- c("c1","c2","c3","c4")
+names(estabilidadLaboral) <- n
+names(edad)               <- n
+names(codeudor)           <- n
+names(obligaciones)       <- n
+names(capacidadCreditica) <- n
+names(salario)            <- n
+names(patrimonio)         <- n
+
+
+#Función para todos los criterios con opciones de 1 a 5
+graf<-function(v,titulo,vEjex, ejexnombre,tipo){
+  
+  f <- c(0,0,0,0,0)
+  names(f)<- vEjex
+  
+  for( i in 1:(length(v)) ){
+    
+    if(v[i]==1) f[1]= f[1]+1
+    else{
+      if(v[i]==2) f[2]= f[2]+1
+      else{
+        if(v[i]==3) f[3]= f[3]+1
+        else{
+          if(v[i]==4) f[4]= f[4]+1
+          else{
+            if(v[i]==5) f[5]= f[5]+1
+          }
+        }
+      }
+    }
+  }
+  if (tipo=="barras")return(barplot(f,  col= c("green", "blue", "pink", "yellow","orange","purple"),xlab=ejexnombre, ylab="Número de Clientes",main=titulo))
+  else
+    return(pie(f,  col= c("green", "blue", "pink", "yellow","orange","purple"),xlab=ejexnombre, ylab="Número de Clientes",main=titulo))
+}
+
+
+#Función para todos los criterios con opciones de 1 y 2
+graf2<-function(v,titulo,vEjex, ejexnombre,tipo){
+  
+  f <- c(0,0)
+  names(f)<- vEjex
+  
+  for( i in 1:(length(v)) ){
+    
+    if(v[i]==1) f[1]= f[1]+1
+    else{
+      if(v[i]==2) f[2]= f[2]+1
+    }
+  }
+  if (tipo=="barras")return(barplot(f,  col= c("green","red"),xlab=ejexnombre, ylab="Número de Clientes",main=titulo))
+  else
+    return(pie(f,  col= c("green", "red"),xlab=ejexnombre, ylab="Número de Clientes",main=titulo))
+}
+
+
+#Gráficas: de barras y torta
+
+w<-c("18-25","26-35" , "36-45" , "46-55", ">55")
+graf(edad,"Frecuencia de clientes por rango de edad",w, "Rango de edades","barras")
+graf(edad,"Frecuencia de clientes por rango de edad",w, "Rango de edades","torta")
+
+w<-c("Ninguna","Pocas", "Mas o menos", "Muchas", "Muchísimas" )
+graf(obligaciones,"Frecuencia por nivel de obligaciones pendientes",w, "Niveles de obligaciones pendientes","barras")
+graf(obligaciones,"Frecuencia por nivel de obligaciones pendientes",w, "Niveles de obligaciones pendientes","torta")
+
+w<-c("Ninguna","Baja", "Media", "Buena", "Excelente")
+graf(capacidadCreditica,"Frecuencia por niveles de capacidad crediticia ",w, "Nivel de capacidad crediticia","barras")
+graf(capacidadCreditica,"Frecuencia por niveles de capacidad crediticia ",w, "Nivel de capacidad crediticia","torta")
+
+w <- c("1-1.5","1.5-3","3-4","4-5",">5")
+graf(salario,"Frecuencia por valor del salario",w, "Intervalos de salario, en SMLV","barras")
+graf(salario,"Frecuencia por valor del salario",w, "Intervalos de salario, en SMLV","torta")
+
+w<-c("<50", "50-100", "100-150","150-400",">400")
+graf(patrimonio,"Frecuencia por valor del patrimonio",w,"Intervalos de patrimonio, en millones de pesos","barras")
+graf(patrimonio,"Frecuencia por valor del patrimonio",w,"Intervalos de patrimonio, en millones de pesos","torta")
+
+w<-c("Con trabajo estable","Sin trabajo estable")
+graf2(estabilidadLaboral,"Frecuencia por estado de estabilidad laboral",w,"Estado de trabajo","barras")
+graf2(estabilidadLaboral,"Frecuencia por estado de estabilidad laboral",w,"Estado de trabajo","torta")
+
+w<-c("Con codeudor","Sin codeudor")
+graf2(codeudor,"Frecuencia por requisito de codeudor",w,"Posesión de codeudor","barras")
+graf2(codeudor,"Frecuencia por requisito de codeudor",w,"Posesión de codeudor","torta")
+
+
+
+
